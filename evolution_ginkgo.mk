ddf453e5d5a5c617e50d77a46e446716b18c9efb
madmax7896 <thegreatsagar123@gmail.com>
2021-01-05 15:36:08 +0000

    ginkgo: Switch to Redfin's Jan 2021 fingerprint

--- evolution_ginkgo.mk
+++ evolution_ginkgo.mk
@@ -38,8 +38,8 @@ PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ1A.201205.010 6953398 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ1A.210105.003 7005429 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
