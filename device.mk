52dfacb763522a4c48a147d76b7933b3768db310
Lunarixus <Nathanb644@hotmail.co.uk>
2021-02-13 07:42:03 +0000

    ginkgo: Remove packages at build time

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.xiaomi_parts.rc
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
